063edf84dc00ce4e4f248b74b22f821b390a44d8
Truong Nguyen <ntt261298@gmail.com>
2019-12-14 19:28:39 +0700

    test: Test cors

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,26 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import Header from './Header';
 import Importer from './Importer';
 import Exporter from './Exporter';
 import Warehouse from './Warehouse';
 
-function Home() {  
+function Home() {
     // Change role here for testing
     const [role, setRole] = useState('exporter');
+
+    useEffect(async () => {
+      const data = {
+        username: 'truong',
+        password: '123',
+        role: 'IMPORTER',
+      }
+      const response = await fetch('http://178.128.217.110:8164/user', {
+        method: 'POST',
+        body: JSON.stringify(data),
+      });
+      const res = await response.json();
+      console.log(res);
+    }, []);
 
     return (
         <Fragment>
